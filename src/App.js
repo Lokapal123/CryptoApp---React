2c4ab5ab8b8fc4258d30f5621fa47fbc3eba08ec
Lokapal02923 <20l31ao5l3@gmail.com>
2024-02-06 16:53:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
 
 function App() {
+  const [search, setsearch] = useState("");
+  const [currency, setcurrency] = useState([]);
+
+  useEffect( ()=> {
+      axios.get('https://openapiv1.coinstats.app/coins',{
+        headers: {'X-API-KEY' : 'qlcW8P4yXYiBck/2lKJziOdo2fHyaox3FwAwURsgIGM='}
+      }).then(res => setcurrency(res.data.result))
+       .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Crypto Currency App</h2>
+      <input type="text" placeholder="Search..." onChange={(e) => setsearch(e.target.value)} />
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Market Cap</th>
+            <th>Price</th>
+            <th>Available Supply</th>
+            <th>Volume(24hr)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currency.filter((val) =>{
+            return val.name.toLowerCase().includes(search.toLowerCase())
+          }).map((val) => {
+            return <tr>
+              <td className="rank">{val.rank}</td>
+              <td className="logo">
+                <a href={val.websiteUrl}>
+                  <img src={val.icon} alt =""/>
+                </a>
+                <p>{val.name}</p>
+              </td>
+              <td className="symbol">
+                {val.symbol}
+              </td>
+              <td>${val.marketCap}</td>
+              <td>${val.price.toFixed(2)}</td>
+              <td>{val.availableSupply}</td>
+              <td>{val.volume.toFixed(2)}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
